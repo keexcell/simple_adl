579300f52eda71d26e9f31718c258f0bd098b104
Kabelo-T <kabelo@uchicago.edu>
2025-02-13 00:37:46 +0000

    added search function

--- simple_adl/search.py
+++ simple_adl/search.py
@@ -50,23 +50,6 @@ def cut_isochrone_path(g, r, g_err, r_err, isochrone, mag_max, radius=0.1, retur
 
     cut = np.logical_or(cut_1, cut_2)
 
-    ## Cut for horizontal branch
-    #mag_1_hb = isochrone.mag_1[isochrone.stage == isochrone.hb_stage][1:] + isochrone.distance_modulus
-    #mag_2_hb = isochrone.mag_2[isochrone.stage == isochrone.hb_stage][1:] + isochrone.distance_modulus
-
-    #f_isochrone = scipy.interpolate.interp1d(mag_2_hb, mag_1_hb - mag_2_hb, bounds_error=False, fill_value = 999.)
-    #color_diff = np.fabs((g - r) - f_isochrone(r))
-    #cut_4 = (color_diff < np.sqrt(0.1**2 + r_err**2 + g_err**2))
-
-    #f_isochrone = scipy.interpolate.interp1d(mag_1_hb, mag_1_hb - mag_2_hb, bounds_error=False, fill_value = 999.)
-    #color_diff = np.fabs((g - r) - f_isochrone(g))
-    #cut_3 = (color_diff < np.sqrt(0.1**2 + r_err**2 + g_err**2))
-    #
-    #cut_hb = np.logical_or(cut_3, cut_4)
-
-    #cut = np.logical_or(cut, cut_hb)
-
-    #mag_bins = np.arange(17., 24.1, 0.1)
     mag_bins = np.arange(17., mag_max+0.1, 0.1)
     mag_centers = 0.5 * (mag_bins[1:] + mag_bins[0:-1])
     magerr = np.tile(0., len(mag_centers))
@@ -82,24 +65,6 @@ def cut_isochrone_path(g, r, g_err, r_err, isochrone, mag_max, radius=0.1, retur
 def write_output(results_dir, nside, pix_nside_select, best_ra_peak, best_dec_peak, best_r_peak, best_distance_modulus, 
                 n_obs_peak, n_obs_half_peak, n_model_peak, 
                 best_sig_peak, mc_source_id, mode, outfile):
-    #writer = open(outfile, 'a') # append if exists
-    #for ii in range(0, len(sig_peak_array)):
-    #    # SIG, RA, DEC, MODULUS, r, n_obs, n_model, mc_source_id
-    #    writer.write('{:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}\n'.format(sig_peak_array[ii], 
-    #                                                                                                                     ra_peak_array[ii], 
-    #                                                                                                                     dec_peak_array[ii], 
-    #                                                                                                                     distance_modulus_array[ii], 
-    #                                                                                                                     r_peak_array[ii],
-    #                                                                                                                     n_obs_peak_array[ii],
-    #                                                                                                                     n_obs_half_peak_array[ii],
-    #                                                                                                                     n_model_peak_array[ii],
-    #                                                                                                                     mc_source_id_array[ii]))
-    #data = [tuple(row) for row in np.stack([sig_peak_array, ra_peak_array, dec_peak_array, distance_modulus_array, r_peak_array, n_obs_peak_array, n_obs_half_peak_array, n_model_peak_array, mc_source_id_array], axis=-1)]
-    #arr = np.array(data, dtype=[('SIG', float), ('RA', float), ('DEC', float), ('MODULUS', float), ('R', float), ('N_OBS', float), ('N_OBS_HALF', float ('N_MODEL', float), ('MC_SOURCE_ID', int))])
-    #np.save(outfile, arr)
-    #f = open(os.path.join(results_dir,outfile), 'ab')
-    #np.savetxt(f, arr, delimiter=',')
-    #f.close()
     
     #saving only the best
     data = np.array([best_sig_peak, best_ra_peak, best_dec_peak, best_distance_modulus, best_r_peak, n_obs_peak, n_obs_half_peak, n_model_peak, mc_source_id]) 
@@ -119,11 +84,6 @@ def search_by_distance(survey, region, distance_modulus, iso_sel, extension=None
     in depth. Then compute the local field density using a small annulus 
     around each individual hotspot, e.g., radius 0.3 to 0.5 deg.
     """
-
-    #print('Distance = {:0.1f} kpc (m-M = {:0.1f})'.format(distanceModulusToDistance(distance_modulus), distance_modulus))
-
-    #print('{} objects left after isochrone cut...'.format(len(data)))
-
     if (len(region.data[iso_sel]) == 0):
         return [], [], [], [], [], [], [], []
 
@@ -167,6 +127,128 @@ def search_by_distance(survey, region, distance_modulus, iso_sel, extension=None
 
     return ra_peak_array, dec_peak_array, r_peak_array, sig_peak_array, distance_modulus_array, n_obs_peak_array, n_obs_half_peak_array, n_model_peak_array
 
+def search(mcid, position, survey, merged_data, sims_at_pos, iso_survey='lsst', save=False, verbose=True):
+    """
+    """
+    ### the iso_survey argument is because I was using the wrong isochrone for so long, now it is needed for testing
+    
+    if verbose: print('Searching for MC_SOURCE_ID ', mcid)
+    ra = position[0]
+    dec = position[1]
+    region = simple_adl.survey.Region(survey, ra, dec)
+    region.data = merged_data
+    distance_modulus = sims_at_pos.loc[sims_at_pos['MC_SOURCE_ID'] == mcid]['DISTANCE_MODULUS']
+    distance_modulus = distance_modulus.values[0]
+    # Create isochrone
+    iso_search = simple_adl.isochrone.Isochrone(survey=iso_survey, # survey.isochrone['survey']
+                                           band_1=survey.band_1.lower(),
+                                           band_2=survey.band_2.lower(),
+                                           age=12.0, #survey.isochrone['age'],
+                                           metallicity=0.00010, #survey.isochrone['metallicity'],
+                                           distance_modulus=distance_modulus)
+
+    iso_selection = cut_isochrone_path(region.data[survey.mag_dered_1], 
+                                          region.data[survey.mag_dered_2],
+                                          region.data[survey.mag_err_1],
+                                          region.data[survey.mag_err_2],
+                                          iso_search,
+                                          survey.catalog['mag_max'],
+                                          radius=0.1)
+    results = search_by_distance(survey, region, distance_modulus, iso_selection) 
+    ra_peak_array, dec_peak_array, r_peak_array, sig_peak_array, distance_modulus_array, n_obs_peak_array, n_obs_half_peak_array, n_model_peak_array = np.asarray(results)
+    best_ra_peak, best_dec_peak, best_r_peak, best_distance_modulus, n_obs_peak, n_obs_half_peak, n_model_peak, best_sig_peak = 0, 0, 0, 0, 0, 0, 0, 0
+
+    if mcid: 
+        mc_source_id_array = np.full_like(distance_modulus_array, mcid)
+    else:
+        mc_source_id_array = np.zeros(len(distance_modulus_array))
+
+    # Sort peaks according to significance
+    index_sort = np.argsort(sig_peak_array)[::-1]
+    ra_peak_array = ra_peak_array[index_sort]
+    dec_peak_array = dec_peak_array[index_sort]
+    r_peak_array = r_peak_array[index_sort]
+    sig_peak_array = sig_peak_array[index_sort]
+    distance_modulus_array = distance_modulus_array[index_sort]
+    n_obs_peak_array = n_obs_peak_array[index_sort]
+    n_obs_half_peak_array = n_obs_half_peak_array[index_sort]
+    n_model_peak_array = n_model_peak_array[index_sort]
+    mc_source_id_array = mc_source_id_array[index_sort]
+
+    # Collect overlapping peaks
+    for ii in range(0, len(sig_peak_array)):
+        if sig_peak_array[ii] < 0:
+            continue
+        sep = angsep(ra_peak_array[ii], dec_peak_array[ii], ra_peak_array, dec_peak_array)
+        sig_peak_array[(sep < r_peak_array[ii]) & (np.arange(len(sig_peak_array)) > ii)] = -1.
+
+    # Prune the list of peaks
+    ra_peak_array = ra_peak_array[sig_peak_array > 0.]
+    dec_peak_array = dec_peak_array[sig_peak_array > 0.]
+    r_peak_array = r_peak_array[sig_peak_array > 0.]
+    distance_modulus_array = distance_modulus_array[sig_peak_array > 0.]
+    n_obs_peak_array = n_obs_peak_array[sig_peak_array > 0.]
+    n_obs_half_peak_array = n_obs_half_peak_array[sig_peak_array > 0.]
+    n_model_peak_array = n_model_peak_array[sig_peak_array > 0.]
+    mc_source_id_array = mc_source_id_array[sig_peak_array > 0.]
+    sig_peak_array = sig_peak_array[sig_peak_array > 0.] # Update the sig_peak_array last!
+
+    if sig_peak_array[0] > best_sig_peak:
+        best_sig_peak = sig_peak_array[0]
+        best_ra_peak = ra_peak_array[0]
+        best_dec_peak = dec_peak_array[0]
+        best_r_peak = r_peak_array[0]
+        best_distance_modulus = distance_modulus_array[0]
+        n_obs_peak = n_obs_peak_array[0]
+        n_obs_half_peak = n_obs_half_peak_array[0]
+        n_model_peak = n_model_peak_array[0]
+        mc_source_id = mc_source_id_array[0]
+        
+    if verbose:
+        # show results from all peaks
+        for ii in range(0, len(sig_peak_array)):
+        print('{:0.2f} sigma; (RA, Dec) = ({:0.2f}, {:0.2f}); r = {:0.2f} deg; d = {:0.1f}, mu = {:0.2f} mag, mc_source_id: {:0.2f}'.format(sig_peak_array[ii], 
+                 ra_peak_array[ii], 
+                 dec_peak_array[ii], 
+                 r_peak_array[ii],
+                 distanceModulusToDistance(distance_modulus_array[ii]),
+                 distance_modulus_array[ii],
+                 mc_source_id_array[ii]))
+    else:
+        print('{:0.2f} sigma; (RA, Dec) = ({:0.2f}, {:0.2f}); r = {:0.2f} deg; d = {:0.1f}, mu = {:0.2f} mag, mc_source_id: {:0.2f}'.format(sig_peak_array[0], 
+                 ra_peak_array[0], 
+                 dec_peak_array[0], 
+                 r_peak_array[0],
+                 distanceModulusToDistance(distance_modulus_array[0]),
+                 distance_modulus_array[0],
+                 mc_source_id_array[0]))
+
+    if best_sig_peak < 5.5:
+        print(f'--> {mcid} NOT FOUND')
+        print('---------------------------------')
+    else:   
+        print(f'--> {mcid} FOUND')
+        print('---------------------------------')
+
+    if save:
+        try:
+            if (len(sig_peak_array) > 0):
+                write_output(survey.output['results_dir'], survey.catalog['nside'], region.pix_center, best_ra_peak, best_dec_peak,
+                                best_r_peak, best_distance_modulus, 
+                                n_obs_peak, n_obs_half_peak, n_model_peak, 
+                                best_sig_peak, mcid, 0, outfile)
+            else:
+                print('No significant hotspots found.')
+                nan_array = [np.nan]
+                write_output(survey.output['results_dir'], survey.catalog['nside'], region.pix_center,
+                                 nan_array, nan_array, nan_array, nan_array, 
+                                 nan_array, nan_array, nan_array, nan_array,
+                                 [mc_source_id], 0, outfile)
+        except Exception as e: 
+            print(e)
+            print('Data missing, cannot write to file')
+
+    
 #-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
